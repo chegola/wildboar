809070c9adbc2717e4132e48cc53bc58c52605b3
Unknown <arkhomk@yahoo.com>
2018-07-28 22:55:30 +0700

    Parent able to register Student - initial

--- src/main/webapp/app/entities/parent-student/parent-student-update.component.ts
+++ src/main/webapp/app/entities/parent-student/parent-student-update.component.ts
@@ -6,7 +6,7 @@ import { JhiAlertService } from 'ng-jhipster';
 
 import { IParentStudent } from 'app/shared/model/parent-student.model';
 import { ParentStudentService } from './parent-student.service';
-import { IUser, UserService } from 'app/core';
+import { IUser, UserService, Principal, Account } from 'app/core';
 import { IStudent } from 'app/shared/model/student.model';
 import { StudentService } from 'app/entities/student';
 
@@ -18,16 +18,21 @@ export class ParentStudentUpdateComponent implements OnInit {
     private _parentStudent: IParentStudent;
     isSaving: boolean;
 
-    users: IUser[];
+    // users: IUser[];
+
+    user: IUser;
 
     students: IStudent[];
+
+    account: Account;
 
     constructor(
         private jhiAlertService: JhiAlertService,
         private parentStudentService: ParentStudentService,
         private userService: UserService,
         private studentService: StudentService,
-        private activatedRoute: ActivatedRoute
+        private activatedRoute: ActivatedRoute,
+        private principal: Principal
     ) {}
 
     ngOnInit() {
@@ -35,12 +40,15 @@ export class ParentStudentUpdateComponent implements OnInit {
         this.activatedRoute.data.subscribe(({ parentStudent }) => {
             this.parentStudent = parentStudent;
         });
-        this.userService.query().subscribe(
-            (res: HttpResponse<IUser[]>) => {
-                this.users = res.body;
-            },
-            (res: HttpErrorResponse) => this.onError(res.message)
-        );
+        this.principal.identity().then(account => {
+            this.account = account;
+        });
+        // this.userService.query().subscribe(
+        //     (res: HttpResponse<IUser[]>) => {
+        //         this.users = res.body;
+        //     },
+        //     (res: HttpErrorResponse) => this.onError(res.message)
+        // );
         this.studentService.query().subscribe(
             (res: HttpResponse<IStudent[]>) => {
                 this.students = res.body;
@@ -55,11 +63,20 @@ export class ParentStudentUpdateComponent implements OnInit {
 
     save() {
         this.isSaving = true;
-        if (this.parentStudent.id !== undefined) {
-            this.subscribeToSaveResponse(this.parentStudentService.update(this.parentStudent));
-        } else {
-            this.subscribeToSaveResponse(this.parentStudentService.create(this.parentStudent));
-        }
+        this.userService.find(this.account.login).subscribe(
+            (res: HttpResponse<IUser>) => {
+                this.user = res.body;
+                this.parentStudent.userId = this.user.id;
+                // this.parentStudent.userLogin = this.user.login;
+                if (this.parentStudent.id !== undefined) {
+                    this.subscribeToSaveResponse(this.parentStudentService.update(this.parentStudent));
+                } else {
+                    this.subscribeToSaveResponse(this.parentStudentService.create(this.parentStudent));
+                }
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+
     }
 
     private subscribeToSaveResponse(result: Observable<HttpResponse<IParentStudent>>) {
@@ -92,5 +109,9 @@ export class ParentStudentUpdateComponent implements OnInit {
 
     set parentStudent(parentStudent: IParentStudent) {
         this._parentStudent = parentStudent;
+    }
+
+    isAuthenticated() {
+        return this.principal.isAuthenticated();
     }
 }
